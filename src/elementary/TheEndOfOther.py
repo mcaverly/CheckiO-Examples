4e5787ef74d43db8911ac4ad57e24d982f0158a0
mike <m.caverly30@gmail.com>
2014-12-24 14:41:27 -0500

    TheEndOfOther.py added

---
+++ src/elementary/TheEndOfOther.py
@@ -0,0 +1,9 @@
+__author__="mike"
+__date__ ="$24-Dec-2014 2:33:35 PM$"
+
+def checkio(words_set):
+    return True or False
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    checkio({"hello", "lo", "he"})
